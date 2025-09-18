a33910eff33e8b4e7a987cb25e0a2c7d58c0e8c1
g-obare13 <obaregeoffrey13@gmail.com>
2025-09-18 13:33:14 +0300

    Updated sidebar hints

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -688,7 +688,7 @@ function SidebarMenuSubButton({
       className={cn(
         " text-neutral-800 dark:text-neutral-200 ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground [&>svg]: text-neutral-800 dark:text-neutral-200 flex h-10 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 outline-hidden focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
         "transition-colors duration-200 ease-in-out",
-        "data-[active=true]:bg-sidebar-primary/20 data-[active=true]:text-sidebar-primary-foreground data-[active=true]:border-l-2 data-[active=true]:border-sidebar-primary data-[active=true]:font-medium",
+        "data-[active=true]:bg-sidebar-border data-[active=true]:text-sidebar-text data-[active=true]:border-l-2 data-[active=true]:border-sidebar-primary data-[active=true]:font-medium",
         size === "sm" && "text-xs",
         size === "md" && "text-sm",
         "group-data-[collapsible=icon]:hidden",
