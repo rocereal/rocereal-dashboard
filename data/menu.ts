da2249591f12f4b8194816ee6c6f26d911e75d59
g-obare13 <obaregeoffrey13@gmail.com>
2025-09-24 20:19:15 +0300

    Update menu.ts

--- data/menu.ts
+++ data/menu.ts
@@ -1,7 +1,11 @@
 import {
+  Banknote,
+  Bot,
   BookOpen,
   CalendarDays,
+  Coins,
   FileText,
+  GraduationCap,
   LayoutDashboard,
   LifeBuoy,
   Lock,
@@ -17,8 +21,8 @@ import {
   File,
   FolderKanban,
   MapPinHouse,
+  Users,
 } from "lucide-react";
-import { addScaleCorrector } from "motion/react";
 
 export const menudata = {
   user: {
@@ -36,17 +40,34 @@ export const menudata = {
       isSection: true,
     },
     {
-      title: "Dashboards",
-      url: "#",
-      icon: LayoutDashboard,
-      items: [
-        { title: "AI", url: "/ai" },
-        { title: "CRM", url: "/crm" },
-        { title: "Crypto", url: "/crypto" },
-        { title: "E-Commerce", url: "/ecommerce" },
-        { title: "Education", url: "/education" },
-        { title: "Finance", url: "/finance" },
-      ],
+      title: "AI",
+      url: "/ai",
+      icon: Bot,
+    },
+    {
+      title: "CRM",
+      url: "/crm",
+      icon: Users,
+    },
+    {
+      title: "Crypto",
+      url: "/crypto",
+      icon: Coins,
+    },
+    {
+      title: "E-Commerce",
+      url: "/ecommerce",
+      icon: ShoppingCart,
+    },
+    {
+      title: "Education",
+      url: "/education",
+      icon: GraduationCap,
+    },
+    {
+      title: "Finance",
+      url: "/finance",
+      icon: Banknote,
     },
 
     // ======================
@@ -62,49 +83,49 @@ export const menudata = {
       icon: CalendarDays,
     },
     {
+      title: "Email",
+      url: "/apps/email",
+      icon: Mail,
+    },
+    {
       title: "E-Commerce",
       url: "#",
       icon: ShoppingCart,
       items: [
-        { title: "Products", url: "/apps/ecommerce/products" },
-        {
-          title: "Product Details",
-          url: "/apps/ecommerce/products/prod-001",
-        },
         { title: "Add Product", url: "/apps/ecommerce/add-product" },
-        { title: "Orders", url: "/apps/ecommerce/orders" },
         { title: "Order Details", url: "/apps/ecommerce/orders/ord-001" },
+        { title: "Orders", url: "/apps/ecommerce/orders" },
+        { title: "Product Details", url: "/apps/ecommerce/products/prod-001" },
+        { title: "Products", url: "/apps/ecommerce/products" },
       ],
     },
     {
-      title: "Real Estate",
+      title: "File Manager",
       url: "#",
-      icon: BookOpen,
+      icon: FolderKanban,
       items: [
-        { title: "Properties", url: "/apps/real-estate" },
-        {
-          title: "Property Details",
-          url: "/apps/real-estate/prop-001",
-        },
-        { title: "Add Property", url: "/apps/real-estate/add-property" },
-        {
-          title: "Edit Property",
-          url: "/apps/real-estate/edit-estate/prop-001",
-        },
+        { title: "Add File", url: "/apps/files/add-file" },
+        { title: "File Details", url: "/apps/files/1" },
+        { title: "File Manager", url: "/apps/files" },
       ],
     },
     {
-      title: "Email",
-      url: "/apps/email",
-      icon: Mail,
+      title: "Invoice",
+      url: "#",
+      icon: Receipt,
+      items: [
+        { title: "Add Invoice", url: "/apps/invoice/add-invoice" },
+        { title: "Invoices", url: "/apps/invoice" },
+        { title: "View Invoice", url: "/apps/invoice/INV-2025-001" },
+      ],
     },
     {
       title: "LMS",
       url: "#",
       icon: Notebook,
       items: [
-        { title: "Courses", url: "/apps/lms" },
         { title: "Course Details", url: "/apps/lms/web-development-basics" },
+        { title: "Courses", url: "/apps/lms" },
       ],
     },
     {
@@ -113,23 +134,27 @@ export const menudata = {
       icon: MessageSquare,
     },
     {
+      title: "Real Estate",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        { title: "Add Property", url: "/apps/real-estate/add-property" },
+        {
+          title: "Edit Property",
+          url: "/apps/real-estate/edit-estate/prop-001",
+        },
+        { title: "Properties", url: "/apps/real-estate" },
+        { title: "Property Details", url: "/apps/real-estate/prop-001" },
+      ],
+    },
+    {
       title: "Tasks",
       url: "#",
       icon: FileText,
       items: [
-        { title: "Task List", url: "/apps/tasks" },
-        { title: "Task Details", url: "/apps/tasks/1" },
         { title: "Create Task", url: "/apps/tasks/create" },
-      ],
-    },
-    {
-      title: "File Manager",
-      url: "#",
-      icon: FolderKanban,
-      items: [
-        { title: "File Manager", url: "/apps/files" },
-        { title: "File Details", url: "/apps/files/1" },
-        { title: "Add File", url: "/apps/files/add-file" },
+        { title: "Task Details", url: "/apps/tasks/1" },
+        { title: "Task List", url: "/apps/tasks" },
       ],
     },
     {
@@ -137,22 +162,9 @@ export const menudata = {
       url: "#",
       icon: UserPlus,
       items: [
-        { title: "Users", url: "/apps/users" },
-        { title: "User Details", url: "/apps/users/user_001" },
         { title: "Add User", url: "/apps/users/add-user" },
-      ],
-    },
-    {
-      title: "Invoice",
-      url: "#",
-      icon: Receipt,
-      items: [
-        { title: "Invoices", url: "/apps/invoice" },
-        {
-          title: "View Invoice",
-          url: "/apps/invoice/INV-2025-001",
-        },
-        { title: "Add Invoice", url: "/apps/invoice/add-invoice" },
+        { title: "User Details", url: "/apps/users/user_001" },
+        { title: "Users", url: "/apps/users" },
       ],
     },
 
@@ -168,50 +180,35 @@ export const menudata = {
       url: "#",
       icon: Lock,
       items: [
+        {
+          title: "Centered Forgot Password",
+          url: "/pages/authentication/centered/forgot-password",
+        },
         { title: "Centered Login", url: "/authentication/centered/login" },
         {
           title: "Centered Register",
           url: "/pages/authentication/centered/register",
         },
         {
-          title: "Centered Forgot Password",
-          url: "/pages/authentication/centered/forgot-password",
-        },
-        {
           title: "Centered Reset Password",
           url: "/pages/authentication/centered/reset-password",
         },
         {
-          title: "Minimal Login",
-          url: "/pages/authentication/minimal/login",
+          title: "Minimal Forgot Password",
+          url: "/pages/authentication/minimal/forgot-password",
         },
+        { title: "Minimal Login", url: "/pages/authentication/minimal/login" },
         {
           title: "Minimal Register",
           url: "/pages/authentication/minimal/register",
-        },
-        {
-          title: "Minimal Forgot Password",
-          url: "/pages/authentication/minimal/forgot-password",
         },
         {
           title: "Minimal Reset Password",
           url: "/pages/authentication/minimal/reset-password",
         },
         {
-          title: "Split-Left Login",
-          url: "/pages/authentication/split-left/login",
-        },
-        {
-          title: "Split-Left Register",
-          url: "/pages/authentication/split-left/register",
-        },
-        {
           title: "Split-Left Forgot Password",
           url: "/pages/authentication/split-left/forgot-password",
-        },
-        {
-          title: "Split-Left Reset Password",
-          url: "/pages/authentication/split-left/reset-password",
         },
         {
           title: "Split-Left Login",
@@ -222,12 +219,12 @@ export const menudata = {
           url: "/pages/authentication/split-left/register",
         },
         {
-          title: "Split-Left Forgot Password",
-          url: "/pages/authentication/split-left/forgot-password",
-        },
-        {
           title: "Split-Left Reset Password",
           url: "/pages/authentication/split-left/reset-password",
+        },
+        {
+          title: "Split-Right Forgot Password",
+          url: "/pages/authentication/split-right/forgot-password",
         },
         {
           title: "Split-Right Login",
@@ -238,25 +235,9 @@ export const menudata = {
           url: "/pages/authentication/split-right/register",
         },
         {
-          title: "Split-Right Forgot Password",
-          url: "/pages/authentication/split-right/forgot-password",
-        },
-        {
           title: "Split-Right Reset Password",
           url: "/pages/authentication/split-right/reset-password",
         },
-      ],
-    },
-    // ======================
-    // Onboarding
-    // ======================
-    {
-      title: "Onboarding",
-      url: "#",
-      icon: UserPlus,
-      items: [
-        { title: "Split-Left", url: "/pages/onboarding/split-left/step-one" },
-        { title: "Split-Right", url: "/pages/onboarding/split-right/step-one" },
       ],
     },
     {
@@ -286,6 +267,15 @@ export const menudata = {
       items: [
         { title: "404 Not Found", url: "/pages/errors/404" },
         { title: "Maintenance", url: "/pages/errors/maintenance" },
+      ],
+    },
+    {
+      title: "Onboarding",
+      url: "#",
+      icon: UserPlus,
+      items: [
+        { title: "Split-Left", url: "/pages/onboarding/split-left/step-one" },
+        { title: "Split-Right", url: "/pages/onboarding/split-right/step-one" },
       ],
     },
     {
