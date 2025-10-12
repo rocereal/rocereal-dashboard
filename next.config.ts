1e00e0d0332337fe358a8b2640fcb0d722baacde
g-obare13 <obaregeoffrey13@gmail.com>
2025-10-12 16:07:41 +0300

    Update X-frame

--- next.config.ts
+++ next.config.ts
@@ -26,10 +26,6 @@ const nextConfig: NextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
